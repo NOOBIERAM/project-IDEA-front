8baf75a8bd4ca7d273c6765494d72c6f3eb04f6a
NOOBIERAM <rotsiniainagg@gmail.com>
2026-01-27 16:28:04 +0300

    feat/register

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -82,7 +82,7 @@ const MainPage = () => {
                         </button>
                         <div className="rounded-full p-[2px] bg-gradient-to-br from-pink-500 via-pink-500 to-purple-500 hover:scale-105 transition-transform duration-300">
                             <button className="bg-white px-6 py-3 rounded-full w-full hover:scale-99 transition-transform duration-300">
-                                S'inscrire
+                                Commencer
                             </button>
                         </div>
                     </div>
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import MainPage from "../pages/MainPage"
 import PrivateRoute from "./PrivatesRoutes"
+import RegisterPage from "../pages/RegisterPage"
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<MainPage />} />
+            <Route path="/register" element={<RegisterPage />} />
             <Route path="/logged" element={<PrivateRoute><div>logged</div></PrivateRoute>} />
         </Routes>
     )
