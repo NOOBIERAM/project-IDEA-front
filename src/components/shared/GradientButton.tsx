57dc8e9476f73b487d612313cc28acbf381532b0
NOOBIERAM <rotsiniainagg@gmail.com>
2026-01-29 13:21:12 +0300

    fix/button gradiant

--- src/components/shared/GradientButton.tsx
+++ src/components/shared/GradientButton.tsx
@@ -2,15 +2,16 @@ import type{ ReactNode } from "react";
 
 interface GradientButtonProps {
   type?: "button" | "submit";
+  w1_full? : boolean;
   children: ReactNode;
   onClick?: () => void;
   className?: string;
 }
 
-const GradientButton = ({ children, onClick, type="button", className = "" }: GradientButtonProps) => {
+const GradientButton = ({ children, onClick, type="button", className = "", w1_full=false }: GradientButtonProps) => {
   return (
     <div
-      className={`rounded-full p-[2px] bg-gradient-to-br from-pink-500 via-pink-500 to-purple-500 
+      className={`w${w1_full && "w-full"} rounded-full p-[2px] bg-gradient-to-br from-pink-500 via-pink-500 to-purple-500 
                   hover:scale-105 transition-transform duration-300 inline-block`}
     >
       <button
